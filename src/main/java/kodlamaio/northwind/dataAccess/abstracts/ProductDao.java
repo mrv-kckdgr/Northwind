149307da26362772047040230862be9bc7e93015
Kucukdogru <m42erve@hotmail.com>
2021-05-23 15:20:01 +0300

    Projeye swagger dahil edildi. API' de kontrolü sağlamak için Result yapısı gerçekleştirildi.

--- src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product, Integer> {
-
+	
 }
